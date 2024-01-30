d89c161c9678fcd164aacedd211888490813c952
Nagendra9199 <nagendranaidu545@gmail.com>
2024-01-30 11:54:19 +0530

    Changwe1

--- src/components/Reports/reports.jsx
+++ src/components/Reports/reports.jsx
@@ -8,7 +8,7 @@ function Reports(){
             <div>
                 <div className="reports-dashboard-details-container">
                     <div className="reports-white-background-container">
-                        
+                        <h1>Report</h1>
                     </div>
                 </div>
             </div>
