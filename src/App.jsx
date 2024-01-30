95a1a7fa9102f99582c1bc30a7208e038725b12e
Nagendra9199 <nagendranaidu545@gmail.com>
2024-01-30 15:25:21 +0530

    change3:25

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import './App.css';
 
 import HomePage from './components/HomePage/home';
 import Dashboard from './components/Dashboard/dashboard';
