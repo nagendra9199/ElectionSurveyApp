95160ba5f9372f7095adf6380a7eb4fb42907c76
Nagendra9199 <nagendranaidu545@gmail.com>
2024-01-30 12:11:58 +0530

    change12:11

--- src/components/Reports/reports.jsx
+++ src/components/Reports/reports.jsx
@@ -8,8 +8,6 @@ function Reports(){
             <div>
                 <div className="reports-dashboard-details-container">
                     <div className="reports-white-background-container">
-                        <h1>Report</h1>
-                        <h2>Hii</h2>
                     </div>
                 </div>
             </div>
